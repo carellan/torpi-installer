e705b22f2a307b4128afa41af2e4c34dc85c5a02
Carlos Arellano <carloscam87@gmail.com>
2020-04-29 21:01:36 +0200

    install torpiweb final

--- install_torpiweb.sh
+++ install_torpiweb.sh
@@ -1,4 +1,11 @@
 apt -y install php php-common apache2
 cd /var/www/html
 git clone https://github.com/carellan/torpiweb.git
+
+cat <<EOF >> /etc/sudoers
+
+www-data ALL=NOPASSWD: ALL
+
+EOF
+
 /etc/init.d/apache2 restart
