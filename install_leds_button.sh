d53b314baa83e7ffb23559a6f91d7659ce3b69a3
Carlos Arellano <carloscam87@gmail.com>
2020-04-29 20:39:33 +0200

    install leds button final

--- install_leds_button.sh
+++ install_leds_button.sh
@@ -1,2 +1,4 @@
-git clone https://github.com/carellan/tor_leds_button.git /root
-/root/tor_leds_button/install_all.sh
+cd /root
+git clone https://github.com/carellan/tor_leds_button.git
+cd /root/tor_leds_button
+./install_all.sh
